e37dec8c6567cd038d87406542db54184746f5f8
gongyu <534862547@qq.com>
2022-09-28 11:12:47 +0800

    更新前端界面

---
+++ snyk-frontend/dist/static/js/chunk-2d0b6b46.08c00736.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b6b46"],{"1dfc":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div")},u=[],r={},s=r,a=t("2877"),i=Object(a["a"])(s,c,u,!1,null,"504f8982",null);e["default"]=i.exports}}]);
